7c72397b45f00b1450649daf0bb7f323c28ae6a2
Velonz15422 <santiago15422@gmail.com>
2023-11-13 17:10:48 -0500

    Try to solve cors issue

--- src/main/java/com/pymextore/softwarev/config/Webconfig.java
+++ src/main/java/com/pymextore/softwarev/config/Webconfig.java
@@ -1,21 +1,24 @@
 package com.pymextore.softwarev.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class Webconfig implements WebMvcConfigurer {
+class Webconfig  {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // Reemplaza con el origen permitido de tu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
