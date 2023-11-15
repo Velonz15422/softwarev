f2a8162c8174ab87c332f8a5a470c2b3c1d7405c
Velonz15422 <santiago15422@gmail.com>
2023-11-15 14:38:24 -0500

    Cors

--- src/main/java/com/pymextore/softwarev/config/CorsConfig.java
+++ src/main/java/com/pymextore/softwarev/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
